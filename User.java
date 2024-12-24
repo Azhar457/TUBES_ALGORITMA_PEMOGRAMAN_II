44d0be7c8a8e3dbf6c2ff67ab10bb6d43e63ca11
Azhar457 <azharsss457@gmail.com>
2024-12-24 19:13:52 +0700

    User FIx

--- User.java
+++ User.java
@@ -4,36 +4,71 @@ import java.util.Scanner;
 
 public class User implements java.io.Serializable {
 
-    Character id, pass, nama;
+    // String id, pass, nama;
+    // Integer Saldo;
+
+    char[] id = new char[6];
+    char[] pass = new char[20]; // assuming max length of 20 for password
+    char[] nama = new char[50]; // assuming max length of 50 for name
     Integer Saldo;
 
     public User() {
-        id = "$";
-        pass = "$";
         Saldo = 0;
     }
 
     void Baca() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("USERNAME : ");
-        id = sc.nextLine();
-        System.out.print("PASSWORD : ");
-        pass = sc.nextLine();
+         Scanner sc = new Scanner(System.in);
+        System.out.print("USERNAME (max 6 chars): ");
+        String tempId = sc.nextLine();
+        while (tempId.length() > 6) {
+            System.out.print("USERNAME too long, please enter again (max 6 chars): ");
+            tempId = sc.nextLine();
+        }
+        id = tempId.toCharArray();
+
+        System.out.print("PASSWORD (min 6 chars): ");
+        String tempPass = sc.nextLine();
+        while (tempPass.length() < 6) {
+            System.out.print("PASSWORD too short, please enter again (min 6 chars): ");
+            tempPass = sc.nextLine();
+        }
+        pass = tempPass.toCharArray();
+
+        System.out.print("NAMA: ");
+        String tempNama = sc.nextLine();
+        nama = tempNama.toCharArray();
     }
 
     void Tampil() {
-        System.out.println("USERNAME : " + id);
-        System.out.println("PASSWORD : " + pass);
+        System.out.println("USERNAME : " + new String(id));
+        System.out.println("PASSWORD : " + new String(pass));
+        System.out.println("NAMA : " + new String(nama));
     }
 
     void TampilPass() {
-        System.out.print("PASSWORD : ");
-        System.out.print(pass.charAt(0));
-        int n = pass.length();
+       System.out.print("PASSWORD : ");
+        System.out.print(pass[0]);
+        int n = pass.length;
         for (int i = 1; i < n - 1; i++) {
             System.out.print("*");
         }
-        System.out.println(pass.charAt(n - 1));
+        System.out.println(pass[n - 1]);
+    }
+
+    void tambahSaldo(int jumlah) {
+        Saldo += jumlah;
+    }
+
+    void kurangiSaldo(int jumlah) {
+        if (Saldo >= jumlah) {
+            Saldo -= jumlah;
+        } else {
+            System.out.println("Saldo tidak mencukupi.");
+        }
+    }
+
+    void tampilSaldo() {
+        System.out.println("Saldo: " + Saldo);
     }
 
     public static void main(String[] args) {
@@ -41,5 +76,9 @@ public class User implements java.io.Serializable {
         T.Baca();
         T.Tampil();
         T.TampilPass();
+        T.tambahSaldo(1000);
+        T.tampilSaldo();
+        T.kurangiSaldo(500);
+        T.tampilSaldo();
     }
 }
